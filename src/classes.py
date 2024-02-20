99530361d422c3b2df5dd4c2f155dc930ad63442
Pavel Golubinec <7respawnables7@gmail.com>
2024-02-20 22:35:44 +0300

    13.2

--- src/classes.py
+++ src/classes.py
@@ -9,7 +9,7 @@ class Category:
     def __init__(self, title, description, products):
         self.title = title
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.total_numbers_of_category += 1
         Category.unique_products += 1
@@ -23,6 +23,22 @@ class Category:
     def get_products(self):
         return self.products
 
+    @property
+    def products(self):
+        list_product = []
+        for products in self.__products:
+            list_product.append(f"{products["name"]}, {products["price"]} руб. Остаток: {products["quantity"]} шт.\n")
+
+        return "".join(list_product)
+
+    def add_products(self, value):
+        self.__products.append({
+            "name": value[0],
+            "description": value[1],
+            "price": value[2],
+            "quantity": value[3]
+        })
+
 
 
 
@@ -35,7 +51,7 @@ class Product:
     def __init__(self, title, description, price, quantity_in_stock):
         self.title = title
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity_in_stock = quantity_in_stock
 
     def get_title(self):
@@ -44,8 +60,35 @@ class Product:
     def get_description(self):
         return self.description
 
+    @property
     def get_price(self):
-        return self.price
+        return self._price
 
     def get_quantity_in_stock(self):
         return self.quantity_in_stock
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена не корректно")
+        elif value < self._price:
+            while True:
+                answer = input("Новая цена ниже чем старая, вы уверены что хотите изменить цену (y/n): ").lower()
+                if answer == 'y':
+                    self._price = value
+                    break
+                elif answer == "n":
+                    self._price = self._price
+                    break
+        else:
+            self._price = value
+
+    @classmethod
+    def create_product(cls, new_product):
+        name, description, price, quantity = \
+            (
+                new_product['name'], new_product['description'], new_product['price'], new_product['quantity']
+            )
+
+        return cls(name, description, price, quantity)
+
